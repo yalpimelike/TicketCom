a9e215fd809aff021080edd3d9360fa08ce01c81
Melike Sivrikaya <melike.svrkya@gmail.com>
2024-08-13 14:25:02 +0300

    Initial commit

---
+++ ticket-service/src/main/java/com/melikesivrikaya/ticketservice/TicketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.melikesivrikaya.ticketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketServiceApplication.class, args);
+    }
+
+}
